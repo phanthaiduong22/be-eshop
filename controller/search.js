e3698d97762c85926d970c9077b34815d96279df
Hieu <18120377@student.hcmus.edu.vn>
2021-01-08 19:58:51 +0700

    Duong phan fix loi search ra nhieu ket qua

--- controller/search.js
+++ controller/search.js
@@ -1,8 +1,19 @@
 const { response } = require("express");
 
 const search = (req, res, db) => {
-  db.select("*")
-    .table("Products")
+  db.select('*')
+    .table('Products')
+    .leftJoin("Product_Image", function () {
+      this.on("Product_Image.product_id", "=", "Products.id")
+        //Chi lay 1 anh
+        .andOn(
+          "Product_Image.image_no",
+          "=",
+          db.raw(
+            '(select min(image_no) from "Product_Image" where "Product_Image".product_id = "Products".id)'
+          )
+        );
+    })
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json(err));
 };
